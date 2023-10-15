930870bf11fe970e2306f02f2714e37effe88361
rbazgan <122384399+rbazgan@users.noreply.github.com>
2023-10-15 22:46:10 +0300

    new add

--- web-scraper-api_backend/routes/web-scrapers.js
+++ web-scraper-api_backend/routes/web-scrapers.js
@@ -1,6 +1,45 @@
 const express=require('express')
 const router=express.Router()
+const article=require('../schemas/schema')
 
-//Getting all web-scrapers
+//Getting all articles
+router.get('/', async (req,res) => {
+    try {
+        const articles=await article.find()
+        res.json(articles)
+    } catch (err) {
+        res.status(500).json({message:error.message})
+    }
+   
+})
+ 
+
+//Getting One article 
+router.get('/:id', (req,res) => {
+res.send(req.param.id)
+})
+//Creating one article
+router.post('/', async (req,res) => {
+  const Article=new article({
+    title:req.body.title,
+    short_description:req.body.short_description
+  })
+
+  try {
+    const newArticle=await Article.save()
+    res.status(201).json(newArticle)
+  } catch (err) {
+    res.status(400).json({message:err.message})
+  }
+})
+//Updating one article
+router.patch('/:id', (req,res) => {
+
+}) 
+
+//Deleting one 
+router.delete('/:id', (req,res) => {
+
+}) 
 
 module.exports=router
---
+++ web-scraper-api_backend/schemas/schema.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose')
+
+const articlesBlog=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    short_description:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports=mongoose.model('article', articlesBlog )
